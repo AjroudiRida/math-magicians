80195f577703f935d91c0e45dd910d4fc837ef6c
Mozamel Jawad <mozameljawad@gmail.com>
2023-08-25 13:09:55 +0430

    Update CalculatorBtns.js file

---
+++ src/components/calculatorBtns/CalculatorBtns.js
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import './buttons.css';
+const CalculatorBtns = ({ content, handleClick }) => (
+  <button
+    type="button"
+    className="btn"
+    onClick={handleClick}
+    onKeyDown={handleClick}
+    data-testid="button"
+  >
+    <span>{content}</span>
+  </button>
+);
+CalculatorBtns.propTypes = {
+  content: PropTypes.string.isRequired,
+  handleClick: PropTypes.func.isRequired,
+};
+export default CalculatorBtns;
